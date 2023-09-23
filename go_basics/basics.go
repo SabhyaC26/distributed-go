6110f70953dfa3da916ce8d81f13394e3ccdea39
Sabhya Chhabria <sabhyachhabria@dynamic-oit-ip4-wifirestricted01-10-16-196-141.princeton.edu>
2023-09-23 08:17:22 -0400

    go basics till arrs

---
+++ go_basics/basics.go
@@ -0,0 +1,146 @@
+package main
+
+import "fmt"
+
+// "math"
+
+// const s string = "constant"
+
+func main() {
+
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2D:", twoD)
+
+	// // Switch
+	// i := 2
+	// fmt.Println("Write", i, "as")
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3:
+	// 	fmt.Println("three")
+	// }
+
+	// switch time.Now().Weekday() {
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("It's the weekend")
+	// default:
+	// 	fmt.Println("It's a weekday")
+	// }
+
+	// t := time.Now()
+	// switch {
+	// case t.Hour() < 12:
+	// 	fmt.Println("It's before noon")
+	// default:
+	// 	fmt.Println("It's after noon")
+	// }
+
+	// whatAmI := func(i interface{}) {
+	// 	switch t := i.(type) {
+	// 	case bool:
+	// 		fmt.Println("I'm a bool")
+	// 	case int:
+	// 		fmt.Println("I'm an int")
+	// 	default:
+	// 		fmt.Printf("Don't know what type %T\n", t)
+	// 	}
+	// }
+
+	// whatAmI(true)
+	// whatAmI(1)
+	// whatAmI("hey")
+
+	// // If/Else
+	// if 7%2 == 0 {
+	// 	fmt.Println("7 is even")
+	// } else {
+	// 	fmt.Println("7 is odd")
+	// }
+
+	// if 8%4 == 0 {
+	// 	fmt.Println("8 is divisible by 4")
+	// }
+
+	// if num := 9; num < 0 {
+	// 	fmt.Println(num, "if negative")
+	// } else if num < 10 {
+	// 	fmt.Println(num, "has 1 digit")
+	// } else {
+	// 	fmt.Println(num, "has multiple digits")
+	// }
+
+	// // Loops
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// for j := 7; j <= 9; j++ {
+	// 	fmt.Println(j)
+	// }
+
+	// for {
+	// 	fmt.Println("loop")
+	// 	break
+	// }
+
+	// for n := 0; n <= 5; n++ {
+	// 	if n%2 == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(n)
+	// }
+
+	// // Constants
+	// const n = 50000000
+	// const d = 3e20 / n
+
+	// fmt.Println(d)
+	// fmt.Println(int64(d))
+	// fmt.Println(math.Sin(n))
+
+	// // Variables
+	// var a = "initial"
+	// fmt.Println(a)
+
+	// var b, c int = 1, 2
+	// fmt.Println(b, c)
+
+	// var d = true
+	// fmt.Println(d)
+
+	// var e int
+	// fmt.Println(e)
+
+	// f := "apple"
+	// fmt.Println(f)
+
+	// // Datatypes
+	// fmt.Println("hello-world")
+	// fmt.Println("go" + "lang")
+	// fmt.Println("1+1 =", 1+1)
+	// fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// fmt.Println(true && false)
+	// fmt.Println(true || false)
+	// fmt.Println(!true)
+}
